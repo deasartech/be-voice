81b7971a837b10f31a6d7817786097e8973bc4ff
deasar-js <deasartechnologies@gmail.com>
2022-09-11 19:12:19 +0100

    write patchNoteByID controller that dynamically builds update object depending on values received. updates title and description but increments cheers_count and comments_count. Basic error handling added within controller. Tests for 400 and 200 passing

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
-import NoteModel, { INoteUpdate, NoteUser } from "../models/notes.model";
+import NoteModel, {
+  INoteUpdate,
+  INoteUpdatePatch,
+  NoteUser,
+} from "../models/notes.model";
 import UserModel from "../models/users.model";
 import TopicModel from "../models/topics.model";
+import Realm from "realm";
+import { realm } from "./auth.controller";
 
 // GET All Notes
 export const getNotes = async (req: Request, res: Response) => {
@@ -61,6 +67,59 @@ export const postNote = async (req: Request, res: Response) => {
       res.status(401).send({ msg: "Cannot Post Note: User Does Not Exist" });
     } else if (checkTopicExists === null) {
       res.status(401).send({ msg: "Cannot Post Note: Topic Does Not Exist" });
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).send({ msg: err.message });
+  }
+};
+
+// PATCH Note By ID
+export const patchNoteByID = async (req: Request, res: Response) => {
+  const { note_id } = req.params;
+  const { title, description, cheers_count, comments_count } = req.body;
+  try {
+    // console.log(realm.currentUser.id, "realm user");
+    // const note = await NoteModel.findOne({ _id: note_id });
+    // console.log(note, "note");
+    // if (note.user.uid === realm.currentUser.id) {
+    // }
+    const update: INoteUpdatePatch = {};
+    if ((title || description) && (cheers_count || comments_count)) {
+      res.status(400).send({ msg: "Error Cannot Updated Note" });
+    } else if (title || description) {
+      if (title) {
+        update.title = title;
+      }
+      if (description) {
+        update.description = description;
+      }
+      const updatedNote = await NoteModel.updateOne({ _id: note_id }, update);
+      console.log(updatedNote, "updated note 1");
+      updatedNote.matchedCount !== 0
+        ? res
+            .status(200)
+            .send({ res: updatedNote, msg: "Successfully Updated Note" })
+        : res.status(400).send({ msg: "Error Cannot Updated Note" });
+    } else if (cheers_count || comments_count) {
+      if (cheers_count) {
+        update.cheers_count = cheers_count;
+      }
+      if (comments_count) {
+        update.comments_count = comments_count;
+      }
+      const updatedCount = await NoteModel.updateOne(
+        { _id: note_id },
+        { $inc: update }
+      );
+      console.log(updatedCount, "updated Count");
+      updatedCount.matchedCount !== 0
+        ? res
+            .status(200)
+            .send({ res: updatedCount, msg: "Successfully Updated Note" })
+        : res.status(400).send({ msg: "Error Cannot Updated Note" });
+    } else {
+      res.status(400).send({ msg: "Error Cannot Updated Note" });
     }
   } catch (err) {
     console.log(err.message);
