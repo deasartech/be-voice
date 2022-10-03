17732f6af271bf5d7e00d17a5701ee34b4b9694d
deasar-js <deasartechnologies@gmail.com>
2022-10-03 22:40:40 +0100

    feat: connection to s3 bucket and send temp upload url to client

---
+++ src/services/s3.service.ts
@@ -0,0 +1,33 @@
+import AWS from "aws-sdk";
+import dotenv from "dotenv";
+import crypto from "crypto";
+import { promisify } from "util";
+const randomBytes = promisify(crypto.randomBytes);
+
+dotenv.config();
+
+const region = "eu-west-2";
+const bucketName = "paranote-profile-image-bucket-01";
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+export const s3 = new AWS.S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+  signatureVersion: "v4",
+});
+
+export async function generateUploadUrl() {
+  const rawBytes = await randomBytes(16);
+  const imageName = rawBytes.toString("hex");
+
+  const params = {
+    Bucket: bucketName,
+    Key: imageName,
+    Expires: 60,
+  };
+
+  const uploadUrl = await s3.getSignedUrlPromise("putObject", params);
+  return uploadUrl;
+}
