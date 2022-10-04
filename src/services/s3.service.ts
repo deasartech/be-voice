2843b07c325a431953358f65d8986ebbdcc21f22
deasar-js <deasartechnologies@gmail.com>
2022-10-04 15:19:51 +0100

    comment all out not building

--- src/services/s3.service.ts
+++ src/services/s3.service.ts
@@ -1,32 +1,33 @@
-import AWS from "aws-sdk";
-import dotenv from "dotenv";
+// import AWS from "aws-sdk";
+// import dotenv from "dotenv";
 // import crypto from "crypto";
 // import { promisify } from "util";
 
-dotenv.config();
+// dotenv.config();
 
-export async function generateUploadUrl() {
-  //   const randomBytes = promisify(crypto.randomBytes);
-  //   const rawBytes = await randomBytes(16);
-  //   const imageName = rawBytes.toString("hex");
-  const region = process.env.AWS_REGION;
-  const bucketName = process.env.AWS_S3_BUCKET_NAME;
-  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+// const region = process.env.AWS_REGION;
+// const bucketName = process.env.AWS_S3_BUCKET_NAME;
+// const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+// const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
-  const s3 = new AWS.S3({
-    region,
-    accessKeyId,
-    secretAccessKey,
-    signatureVersion: "v4",
-  });
+// export const s3 = new AWS.S3({
+//   region,
+//   accessKeyId,
+//   secretAccessKey,
+//   signatureVersion: "v4",
+// });
 
-  const params = {
-    Bucket: bucketName,
-    Key: "image name",
-    Expires: 60,
-  };
+// export async function generateUploadUrl() {
+//   const randomBytes = promisify(crypto.randomBytes);
+//   const rawBytes = await randomBytes(16);
+//   const imageName = rawBytes.toString("hex");
 
-  const uploadUrl = await s3.getSignedUrlPromise("putObject", params);
-  return uploadUrl;
-}
+//   const params = {
+//     Bucket: bucketName,
+//     Key: "image name",
+//     Expires: 60,
+//   };
+
+//   const uploadUrl = await s3.getSignedUrlPromise("putObject", params);
+//   return uploadUrl;
+// }
