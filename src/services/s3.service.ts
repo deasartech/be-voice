a92fe8d5b52f8f2f723878efd7ee9dc2fb50bf22
deasar-js <deasartechnologies@gmail.com>
2022-10-04 19:25:31 +0100

    removed test functions

--- src/services/s3.service.ts
+++ src/services/s3.service.ts
@@ -31,28 +31,3 @@
 //   const uploadUrl = await s3.getSignedUrlPromise("putObject", params);
 //   return uploadUrl;
 // }
-
-import mongoose from "mongoose";
-import { MongoMemoryServer } from "mongodb-memory-server";
-
-export async function connectDBForTesting() {
-  try {
-    const mongod = await MongoMemoryServer.create();
-    const dbUri = mongod.getUri();
-    const dbName = "test";
-    await mongoose.connect(dbUri, {
-      dbName,
-      autoCreate: true,
-    });
-  } catch (error) {
-    console.log("DB connect error");
-  }
-}
-
-export async function disconnectDBForTesting() {
-  try {
-    await mongoose.connection.close();
-  } catch (error) {
-    console.log("DB disconnect error");
-  }
-}
