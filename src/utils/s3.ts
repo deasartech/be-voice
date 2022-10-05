4de40454b1f94280e8fcf00d05621da336a7488e
deasar-js <deasartechnologies@gmail.com>
2022-10-05 21:04:13 +0100

    nest aws variables inside generate upload url function

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -5,19 +5,19 @@ import { promisify } from "util";
 
 dotenv.config();
 
-const region = process.env.AWS_REGION;
-const bucketName = process.env.AWS_S3_BUCKET_NAME;
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-
-export const s3 = new AWS.S3({
-  region,
-  accessKeyId,
-  secretAccessKey,
-  signatureVersion: "v4",
-});
-
 export async function generateUploadUrl() {
+  const region = process.env.AWS_REGION;
+  const bucketName = process.env.AWS_S3_BUCKET_NAME;
+  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+  const s3 = new AWS.S3({
+    region,
+    accessKeyId,
+    secretAccessKey,
+    signatureVersion: "v4",
+  });
+
   const randomBytes = promisify(crypto.randomBytes);
   const rawBytes = await randomBytes(16);
   const imageName = rawBytes.toString("hex");
