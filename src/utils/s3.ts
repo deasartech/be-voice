4ba6af5b1fb0930967de6146d70fe39ebf496097
deasar-js <deasartechnologies@gmail.com>
2022-10-05 21:35:56 +0100

    try to move outside function

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -5,18 +5,24 @@ import { promisify } from "util";
 
 dotenv.config();
 
-export async function generateUploadUrl() {
-  const region = process.env.AWS_REGION;
-  const bucketName = process.env.AWS_S3_BUCKET_NAME;
-  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
-  const s3 = new AWS.S3({
-    region,
-    accessKeyId,
-    secretAccessKey,
-    signatureVersion: "v4",
-  });
+const s3 = new AWS.S3({
+  // region,
+  // accessKeyId,
+  // secretAccessKey,
+  signatureVersion: "v4",
+});
+
+export async function generateUploadUrl() {
+  // const region = process.env.AWS_REGION;
+  const bucketName = process.env.AWS_S3_BUCKET_NAME;
+  // const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  // const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
   const randomBytes = promisify(crypto.randomBytes);
   const rawBytes = await randomBytes(16);
